cb2f92adc1e2defb8a67cbfd43a341a815f79de2
AndreaBelardi <abelardi81@gmail.com>
2026-02-20 23:34:59 +0100

    primo commit - app fastapi + pipeline CI/CD

---
+++ app/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"message": "Ciao dal cluster k3s!", "status": "ok"}
+
+@app.get("/health")
+def health():
+    return {"status": "healthy"}
+
+@app.get("/info")
+def info():
+    return {
+        "app": "k8s-lab",
+        "version": "1.0.0",
+        "description": "La mia prima app su Kubernetes"
+    }
+
